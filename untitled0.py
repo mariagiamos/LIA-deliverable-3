b140596bfae5a6fcf3592450de7dd4bc9f7186bf
mariagiamos <mariagiamos07@gmail.com>
2025-11-09 22:20:26 -0500

    initial data inspections
    Gave a small overview and summary using functions such as head(),info(),describe(),nunique(), values_counts() and attributes such as shape and columns.

--- untitled0.py
+++ untitled0.py
@@ -3,4 +3,30 @@ print("deliverable 3")
 import pandas as pd
 data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
 
+#2
+#a)initial data inspection
+#this step is to give a preview of our chosen dataset.
+print(data.head())
+#the head function gives a short preview of the dataset. it prints out the first 5 rows of the data set of all 13 columns.
+print(data.info())
+#the info function prints out the non null count(values that are not empty) in each column. From this short summary we see that Sleep disorders only have 155 values compared to all the  other columns who dont have missing values.
+print(data.describe())
+#the describe function summarizes the statistics of the dataset. It print out the count(values) for the columns, the mean of each column, the standard deviation, the minimum and max values in each column, and lastlt the 25% and 75% quartile range and the median.
+print(data.shape)
+#the shape attribute prints out the number of rows (374) and columns(13). 
+print(data.columns)
+#the column attribute prints out the list of all column titles in the dataset.
+print(data.nunique())
+#the nunique function prints out the unique numbers in each column.
+print(data.value_counts())
+#the value_count functio prints out how many times each unique values appears in each column. 
+
+
+
+
+
+
+
+
+
 
